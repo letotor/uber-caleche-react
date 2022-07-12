2e0b2ece46c39bcb3d22f0ea344129381c0b39b7
David71700 <dadeire@icloud.com>
2022-07-13 00:34:01 +0200

    add menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import map from '../assets/map.png'
+import Selector from './Selector'
 
 const Home = () => {
   return (
+    <>
     <div>
       <img className='object-cover w-full' src={map} alt="map" />
     </div>
+    <div>
+       <Selector/> 
+    </div>
+    </>
   );
 };
+
+
 
 
 export default Home;
