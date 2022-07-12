89720816ce8ee98ea4f01299cf03e06e69343e53
Bricius13 <charifibrice.pro@gmail.com>
2022-07-13 00:34:01 +0200

    footer contact profil

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Contact = () => {
   return (
   <>
-
+<div className="flex flex-col items-center justify-center ">
 <div className="flex items-center justify-center mb-3 mt-36 form-control">
-  <label className="label">
+  <label className=" label">
   </label> 
-  <textarea className="w-screen h-48 textarea textarea-bordered" placeholder="Enter your message"></textarea>
+  <textarea className="w-screen h-48 textarea textarea-bordered " placeholder="Enter your message"></textarea>
   
 </div>
-<div className="flex items-center justify-center btn btn-outline ">
+<div className="flex items-center justify-center w-40 btn btn-outline">
 <button>Send</button>
 </div>
-
+</div>
 
   
 
