3aa49d61946635edd3acfa9134bc7f6483960396
Bricius13 <charifibrice.pro@gmail.com>
2022-07-12 20:37:34 +0200

    navlink footer

--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
+import { Routes, Route } from 'react-router-dom'
+import Login from './components/Login';
+import Profile from './components/Profile';
 
-const app = () => {
+const App = () => {
   return (
     <div>
       <Header />
-      <Home />
-      <Footer />
+      <Routes>
+        
+        <Route path='/' element={<Home />}  />
+         <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route path='/profile' element={<Profile />} />
+      </Routes> 
+        <Footer />
+      
     </div>
   );
 };
 
-export default app;
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+
+import { NavLink } from 'react-router-dom'
 import homeImage from '../assets/calecheHome.svg'
 import profileImage from '../assets/account.svg'
 import contactImage from '../assets/contact.svg'
 const Footer = () => {
+
+let activeClassName = "underline"
+
   return (
   
 <div className="btm-nav">
   <button>
+    <NavLink to='/' className={({isActive}) => isActive ? activeClassName : undefined}>
     <img src={homeImage} alt="order" className="w-6 h-6" /><span className="text-xs btm-nav-label">Order</span>
+    </NavLink>
   </button>
   <button className="active">
-    <img src={profileImage} alt="profile" className="w-6 h-6" /><span className="text-xs btm-nav-label">Account</span>
+    <NavLink to='/profile' className={({isActive}) => isActive ? activeClassName : undefined}>
+    <img src={profileImage} alt="profile" className="w-6 h-6 " /><span className="text-xs btm-nav-label">Account</span>
+    </NavLink>
   </button>
   <button>
-  <img src={contactImage} alt="contact" className="w-6 h-6" /><span className="text-xs btm-nav-label">Contact</span>
+  <NavLink to='/login' className={({isActive}) => isActive ? activeClassName : undefined}>
+  <img src={contactImage} alt="login" className="w-6 h-6" /><span className="text-xs btm-nav-label">Login</span>
+  </NavLink>
   </button>
 </div>
   );
