4aadd5584dda377a88349210ba26da40e343d26a
Bricius13 <charifibrice.pro@gmail.com>
2022-07-13 01:05:54 +0200

    maj index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './app.css';
+import './App.css';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(<BrowserRouter><App /> </BrowserRouter>, document.getElementById('root'));
