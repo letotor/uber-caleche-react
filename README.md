ccc9d1896fea0600bdc18aa3fae7c130eeb11649
letotor <v.degermain@gmail.com>
2022-07-12 12:38:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-react-cra-ak39g1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-cra-ak39g1)
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = (props) => {
+  return (
+    <div>
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+};
+export default App;
